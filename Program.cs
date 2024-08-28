4e709e5378c28478226248589665cb0a79c64f09
Tw212 <Tewe07001@utb.vaxjo.se>
2024-08-28 11:16:53 +0200

    programmering 1 uppgift

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Tracing;
+Console.WriteLine("write two numbers");
+string text = Console.ReadLine();
+string text2 = Console.ReadLine();
+int tal = int.Parse(text);
+int tal2 = int.Parse(text2);
+Console.WriteLine(tal + tal2);
+Console.WriteLine(tal - tal2);
+
+
+/*Console.WriteLine("Write your first and last name");
+string text = Console.ReadLine();
+string text2 = Console.ReadLine();
+Console.WriteLine(text + " " + text2);
+*/
+
